bd1f3824e02c388a7a6a2d1c690fc56662a8e100
Claus Due <claus@wildside.dk>
2013-03-10 23:31:39 +0100

    [TER] 0.9.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
 /***************************************************************
- * Extension Manager/Repository config file for ext "gits".
+ * Extension Manager/Repository config file for ext "tweetnews".
  *
- * Auto generated 05-12-2012 21:13
+ * Auto generated 10-03-2013 23:31
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'Tweet News: Automatic tweeting of published EXT:news items',
 	'description' => 'Use the Twitter API to tweet about news items added in Georg Ringers EXT:news extension. Minimal configuration - no database storage used.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.0.1',
+	'version' => '0.9.0',
 	'dependencies' => 'cms,extbase,fluid,news,flux',
 	'conflicts' => '',
 	'priority' => '',
@@ -30,21 +30,24 @@ $EM_CONF[$_EXTKEY] = array (
 	'author' => 'Claus Due',
 	'author_email' => 'claus@wildside.dk',
 	'author_company' => 'Wildside A/S',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
-	'constraints' =>
-	array (
-		'depends' =>
-		array (
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
 			'cms' => '',
 			'extbase' => '',
 			'fluid' => '',
 			'news' => '',
 			'flux' => '',
 		),
-		'conflicts' => array(),
-		'suggests' => array (),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
 	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:7:{s:17:"ext_localconf.php";s:4:"253f";s:14:"ext_tables.php";s:4:"5d20";s:9:"README.md";s:4:"1831";s:46:"Classes/Provider/NewsConfigurationProvider.php";s:4:"55c8";s:35:"Classes/Service/SettingsService.php";s:4:"dd68";s:34:"Configuration/TypoScript/setup.txt";s:4:"fe57";s:30:"Resources/Contrib/CodeBird.php";s:4:"6bcb";}',
 );
 
 ?>
