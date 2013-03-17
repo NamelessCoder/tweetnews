e4e7f9ae11a185cde432a77e2e3b487ed1532af4
Claus Due <claus@wildside.dk>
2013-03-17 23:47:01 +0100

    [TER] 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "tweetnews".
  *
- * Auto generated 10-03-2013 23:31
+ * Auto generated 17-03-2013 23:46
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Use the Twitter API to tweet about news items added in Georg Ringers EXT:news extension. Minimal configuration - no database storage used.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.9.0',
+	'version' => '1.0.0',
 	'dependencies' => 'cms,extbase,fluid,news,flux',
 	'conflicts' => '',
 	'priority' => '',
@@ -47,7 +47,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:7:{s:17:"ext_localconf.php";s:4:"253f";s:14:"ext_tables.php";s:4:"5d20";s:9:"README.md";s:4:"1831";s:46:"Classes/Provider/NewsConfigurationProvider.php";s:4:"55c8";s:35:"Classes/Service/SettingsService.php";s:4:"dd68";s:34:"Configuration/TypoScript/setup.txt";s:4:"fe57";s:30:"Resources/Contrib/CodeBird.php";s:4:"6bcb";}',
+	'_md5_values_when_last_written' => 'a:7:{s:17:"ext_localconf.php";s:4:"253f";s:14:"ext_tables.php";s:4:"5d20";s:9:"README.md";s:4:"1831";s:46:"Classes/Provider/NewsConfigurationProvider.php";s:4:"ddf6";s:35:"Classes/Service/SettingsService.php";s:4:"3c7f";s:34:"Configuration/TypoScript/setup.txt";s:4:"1ad4";s:30:"Resources/Contrib/CodeBird.php";s:4:"6bcb";}',
 );
 
 ?>
